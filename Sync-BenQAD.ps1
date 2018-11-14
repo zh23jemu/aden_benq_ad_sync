716285e80156791d65d43e2f95c301d029e2ce6e
zh23jemu <41720280+zh23jemu@users.noreply.github.com>
2018-11-14 14:46:55 +0800

    added Cadena sync2ad

--- Sync-BenQAD.ps1
+++ Sync-BenQAD.ps1
@@ -50,35 +50,67 @@ get-pssession | remove-pssession
 ## Prepare BenQ                         #####
 #############################################
 
-$query = "select * from [dbo].[v_OutlookData]" 
-#$query = "select * from [dbo].[v_OutlookData] where Email = 'leon.wang@adenservices.com'" 
-$connectionString = "Data Source=192.168.0.97;Initial Catalog=eHR;User Id=exchange;Password=exchange"
+$connectionStringBenq = "Data Source=97VMDBSERVER.CHOADEN.COM;Initial Catalog=eHR;User Id=exchange;Password=exchange"
+$connectionStringCadena = "Data Source=ccnsvwhqdwh02.choaden.com;Initial Catalog=BI_HR;User Id=weaden;Password=adenweaden@123"
+
+$queryBenq = "select EMPLOYEEID,FIRSTNAME,LASTNAME,DisplayName,Initials,Email,Dept,Region,JobTitle,OFFICEPHONE,EXT,MobilePhone,ReportToID,'BENQ' as source
+	from [dbo].[v_OutlookData]
+    where (leavedate='' or LeaveDate is null) and (OutDate='' or OutDate is null)" 
+$queryCadena = "select EmployeeID,FirstName,LastName,Name as Displayname,'' as initials,Email,Department as dept,Region,JobTitle,'' as officephone,'' as ext,'' as mobilephone,SupervisorID as reporttoid,'CADENA' as source 
+    from dbo.HR_EMPS_VN 
+    where EmployeeID is not NULL and EmployeeID <>'' and Email like '%@adenservices.com' and EmployeeStatus = 'Active'"
+
 
 #############################################
 ## Prepare Log File						#####
 #############################################
 
 $batchNo =  Get-Date -Format 'yyyyMMddHH'
-$runningLog = "C:\log\SyncBenqAD\RunningLog.log"
-$syncBenqADLog = "C:\log\SyncBenqAD\SyncBenqAD" + $batchNo + ".log"
+$LogPath = "C:\log\SyncBenqAD\"
+$runningLog = $LogPath + "RunningLog.log"
+$syncBenqADLog = $LogPath + "SyncBenqAD" + $batchNo + ".log"
+if (!(Test-Path $LogPath))
+{
+    mkdir $LogPath
+}
 
 ########## Connect to BenQ DB ############
 
-$connection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+$connectionBenq = New-Object -TypeName System.Data.SqlClient.SqlConnection
 
-$connection.ConnectionString = $connectionString
-$command = $connection.CreateCommand()
-$command.CommandText = $query
-$adapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $command
-$dataset = New-Object -TypeName System.Data.DataSet
-$adapter.Fill($dataset)
-$table=$dataset.Tables[0]
+$connectionBenq.ConnectionString = $connectionStringBenq
+$commandBenq = $connectionBenq.CreateCommand()
+$commandBenq.CommandText = $queryBenq
+$adapterBenq = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $commandBenq
+$datasetBenq = New-Object -TypeName System.Data.DataSet
+$adapterBenq.Fill($datasetBenq)
+$tableBenq=$datasetBenq.Tables[0]
+
+$connectionBenq.Close()
+
+$connectionCadena = New-Object -TypeName System.Data.SqlClient.SqlConnection
+
+$connectionCadena.ConnectionString = $connectionStringCadena
+$commandCadena = $connectionCadena.CreateCommand()
+$commandCadena.CommandText = $queryCadena
+$adapterCadena = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $commandCadena
+$datasetCadena = New-Object -TypeName System.Data.DataSet
+$adapterCadena.Fill($datasetCadena)
+$tableCadena=$datasetCadena.Tables[0]
+
+$connectionCadena.Close()
+
+$allData = $tableBenq.Rows + $tableCadena.Rows
+
 $count = 1
 
 "" > $runningLog
 
+#$allData | ogv
+#$allData | Export-Csv d:\cadena.csv
+
 ########### Traverse the table ############
-foreach ($item in $table.Rows)
+foreach ($item in $allData)
 {
 	$employeeId = HandleNull($item.EMPLOYEEID.Trim())
 	$firstName = HandleNull($item.FIRSTNAME.Trim())
@@ -92,25 +124,26 @@ foreach ($item in $table.Rows)
 	$officePhone = HandleNull($item.OFFICEPHONE)
 	$ext = HandleNull($item.EXT)
 	$mobilePhone = HandleNull($item.MobilePhone)
-	$outDate = HandleNull($item.OutDate)
-	$leaveDate = HandleNull($item.LeaveDate)
+	#$outDate = HandleNull($item.OutDate)
+	#$leaveDate = HandleNull($item.LeaveDate)
 	$reportToId = HandleNull($item.ReportToID)
+    $ou = HandleNull($item.source)
 	$telephoneNumber = TelephoneNumber -officePhone $officePhone -ext $ext
 	$name = $email.substring(0, $email.IndexOf("@")).Trim()
 
-	# check manage name
+	# check manager name
 	$managerName = $null
 	if ($reportToId -ne $null)
 	{	
 		$managerName = get-aduser -Filter {EmployeeID -eq $reportToId} -ErrorAction SilentlyContinue | select samAccountName
 	}
 
-	# check if user should be set as hidden from Exchange address list
+	<# check if user should be set as hidden from Exchange address list
 	$isHidden = $False
 	if(($jobTitle -eq $null -and $region -eq $null) -or $outDate -ne $null -or $leaveDate -ne $null)
 	{
 		$isHidden = $true 
-	}
+	}#>
 
 	# filter user by EmployeeID
 	$adAccount = Get-ADUser -Filter {EmployeeID -eq $employeeId} | Select-Object samAccountName,UserPrincipalName
@@ -137,38 +170,38 @@ foreach ($item in $table.Rows)
 	# user already exits
 	elseif ($sam -ne $null)
 	{
-		if($isHidden -eq $True)
+		<#if($isHidden -eq $True)
 		{
 			Set-ADUser $sam -Replace @{msExchHideFromAddressLists=$True} 
 			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`thas been set as hidden."
 			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`thas been set as hidden." >> $runningLog
 		}
 		else
-		{
+		{#>
 			# update AD user
-			Set-ADUser $sam `
-				-SurName $lastName `
-				-GivenName $firstName `
-				-DisplayName $displayName `
-				-Department $dept `
-				-Office $region `
-				-Title $jobTitle `
-				-OfficePhone $telephoneNumber `
-				-MobilePhone $mobilePhone `
-				-Initials $initials `
-				-userprincipalname $email `
-				-EmailAddress $email `
-				-Manager $managerName `
-				-Replace @{msExchHideFromAddressLists=$False}
+	    Set-ADUser $sam `
+		    -SurName $lastName `
+		    -GivenName $firstName `
+		    -DisplayName $displayName `
+		    -Department $dept `
+		    -Office $region `
+		    -Title $jobTitle `
+		    -OfficePhone $telephoneNumber `
+		    -MobilePhone $mobilePhone `
+		    -Initials $initials `
+		    -userprincipalname $email `
+		    -EmailAddress $email `
+		    -Manager $managerName `
+		    -Replace @{msExchHideFromAddressLists=$False}
 
-			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated."
-			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated." >> $runningLog
-		}
+	    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated."
+	    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated." >> $runningLog
+		#}
 	}
 	else 
 	{
-		if ($isHidden -eq $False)
-		{
+		#if ($isHidden -eq $False)
+		#{
 			$useraccount = Get-ADUser -Filter {sAMAccountName  -eq $name}
 			$useraccount2 = Get-ADUser -Filter {userPrincipalName  -eq $email}
 
@@ -186,7 +219,7 @@ foreach ($item in $table.Rows)
 			}
 			else
 			{
-				$ouPath = "OU=BENQ,OU=ADEN-Users,DC=CHOADEN,DC=COM"
+				$ouPath = "OU="+ $ou +",OU=ADEN-Users,DC=CHOADEN,DC=COM"
 				if([adsi]::Exists("LDAP://$ouPath"))
 				{
 					New-ADUser $name `
@@ -218,12 +251,12 @@ foreach ($item in $table.Rows)
 					break
 				}
 			}
-		}
-		else 
+		#}
+		<#else 
 		{
 			$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tADUser does not exist, but will be not created because user is set as hidden."
 			$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tADUser does not exist, but will be not created because user is set as hidden." >> $runningLog
-		}
+		}#>
 	}
 	$count ++
 }
