2e1441b447c80147a5fba51c032edc0e909a6c40
zh23jemu <41720280+zh23jemu@users.noreply.github.com>
2018-11-23 13:06:51 +0800

    added exclusion list

--- Sync-BenQAD.ps1
+++ Sync-BenQAD.ps1
@@ -69,6 +69,7 @@ $batchNo =  Get-Date -Format 'yyyyMMddHH'
 $LogPath = "C:\log\SyncBenqAD\"
 $runningLog = $LogPath + "RunningLog.log"
 $syncBenqADLog = $LogPath + "SyncBenqAD" + $batchNo + ".log"
+$exclusion = Get-Content ($ScriptFolder + "\exclusion\Sync-HRAD.txt")
 if (!(Test-Path $LogPath))
 {
     mkdir $LogPath
@@ -112,153 +113,132 @@ $count = 1
 ########### Traverse the table ############
 foreach ($item in $allData)
 {
-	$employeeId = HandleNull($item.EMPLOYEEID.Trim())
-	$firstName = HandleNull($item.FIRSTNAME.Trim())
-	$lastName = HandleNull($item.LASTNAME.Trim())
-	$displayName = HandleNull($item.DisplayName.Trim())
-	$initials = HandleNull($item.initials.Trim())
-	$email = HandleNull($item.Email.Trim())
-	$dept = HandleNull($item.Dept.Trim())
-	$region = HandleNull($item.Region.Trim())
-	$jobTitle = HandleNull($item.jobTitle.Trim())
-	$officePhone = HandleNull($item.OFFICEPHONE)
-	$ext = HandleNull($item.EXT)
-	$mobilePhone = HandleNull($item.MobilePhone)
-	#$outDate = HandleNull($item.OutDate)
-	#$leaveDate = HandleNull($item.LeaveDate)
-	$reportToId = HandleNull($item.ReportToID)
-    $ou = HandleNull($item.source)
-	$telephoneNumber = TelephoneNumber -officePhone $officePhone -ext $ext
-	$name = $email.substring(0, $email.IndexOf("@")).Trim()
+    $email = HandleNull($item.Email.Trim())
+    if ($exclusion -notcontains $email)
+    {
+	    $employeeId = HandleNull($item.EMPLOYEEID.Trim())
+	    $firstName = HandleNull($item.FIRSTNAME.Trim())
+	    $lastName = HandleNull($item.LASTNAME.Trim())
+	    $displayName = HandleNull($item.DisplayName.Trim())
+	    $initials = HandleNull($item.initials.Trim())
+	    $dept = HandleNull($item.Dept.Trim())
+	    $region = HandleNull($item.Region.Trim())
+	    $jobTitle = HandleNull($item.jobTitle.Trim())
+	    $officePhone = HandleNull($item.OFFICEPHONE)
+	    $ext = HandleNull($item.EXT)
+	    $mobilePhone = HandleNull($item.MobilePhone)
+	    $reportToId = HandleNull($item.ReportToID)
+        $ou = HandleNull($item.source)
+	    $telephoneNumber = TelephoneNumber -officePhone $officePhone -ext $ext
+	    $name = $email.substring(0, $email.IndexOf("@")).Trim()
 
-	# check manager name
-	$managerName = $null
-	if ($reportToId -ne $null)
-	{	
-		$managerName = get-aduser -Filter {EmployeeID -eq $reportToId} -ErrorAction SilentlyContinue | select samAccountName
-	}
-
-	<# check if user should be set as hidden from Exchange address list
-	$isHidden = $False
-	if(($jobTitle -eq $null -and $region -eq $null) -or $outDate -ne $null -or $leaveDate -ne $null)
-	{
-		$isHidden = $true 
-	}#>
-
-	# filter user by EmployeeID
-	$adAccount = Get-ADUser -Filter {EmployeeID -eq $employeeId} | Select-Object samAccountName,UserPrincipalName
-	if ($adAccount.Count -ge 2)
-	{
-		if (($adAccount | where {$_.enabled -eq $true}).Count -ge 2)
-		{	
-			$employeeId + "`t" + $email + "`thas more than one enabled AD accounts." >> $syncBenqADLog
-		}
-		$sam = $adAccount[0].SamAccountName
-	}
-	else 
-	{
-		$sam = $adAccount.SamAccountName
-	}
-
-
-	if ($email -notlike "*@adenservices.com" -and $email -notlike "*@axingservices.com") 
-	{
-		$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tEmail format error. AD user will not be created or updated."
-		$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tEmail format error. AD user will not be created or updated." >> $runningLog
-		$employeeId + "`t" + $name + "`t" + $email + "`tEmail format error. AD user will not be created or updated." >> $syncBenqADLog
-	}
-	# user already exits
-	elseif ($sam -ne $null)
-	{
-		<#if($isHidden -eq $True)
-		{
-			Set-ADUser $sam -Replace @{msExchHideFromAddressLists=$True} 
-			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`thas been set as hidden."
-			$count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`thas been set as hidden." >> $runningLog
-		}
-		else
-		{#>
+	    # check manager name
+	    $managerName = $null
+	    if ($reportToId -ne $null)
+	    {	
+		    $managerName = get-aduser -Filter {EmployeeID -eq $reportToId} -ErrorAction SilentlyContinue | select samAccountName
+	    }
+	    # filter user by EmployeeID
+	    $adAccount = Get-ADUser -Filter {EmployeeID -eq $employeeId} | Select-Object samAccountName,UserPrincipalName
+	    if ($adAccount.Count -ge 2)
+	    {
+		    if (($adAccount | where {$_.enabled -eq $true}).Count -ge 2)
+		    {	
+			    $employeeId + "`t" + $email + "`thas more than one enabled AD accounts." >> $syncBenqADLog
+		    }
+		    $sam = $adAccount[0].SamAccountName
+	    }
+	    else 
+	    {
+		    $sam = $adAccount.SamAccountName
+    	}
+	    if ($email -notlike "*@adenservices.com" -and $email -notlike "*@axingservices.com") 
+	    {
+		    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tEmail format error. AD user will not be created or updated."
+		    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tEmail format error. AD user will not be created or updated." >> $runningLog
+		    $employeeId + "`t" + $name + "`t" + $email + "`tEmail format error. AD user will not be created or updated." >> $syncBenqADLog
+	    }
+	    # user already exits
+	    elseif ($sam -ne $null)
+	    {
 			# update AD user
-	    Set-ADUser $sam `
-		    -SurName $lastName `
-		    -GivenName $firstName `
-		    -DisplayName $displayName `
-		    -Department $dept `
-		    -Office $region `
-		    -Title $jobTitle `
-		    -OfficePhone $telephoneNumber `
-		    -MobilePhone $mobilePhone `
-		    -Initials $initials `
-		    -userprincipalname $email `
-		    -EmailAddress $email `
-		    -Manager $managerName `
-		    -Replace @{msExchHideFromAddressLists=$False}
+	        Set-ADUser $sam `
+		        -SurName $lastName `
+		        -GivenName $firstName `
+		        -DisplayName $displayName `
+		        -Department $dept `
+		        -Office $region `
+		        -Title $jobTitle `
+		        -OfficePhone $telephoneNumber `
+		        -MobilePhone $mobilePhone `
+		        -Initials $initials `
+		        -userprincipalname $email `
+		        -EmailAddress $email `
+		        -Manager $managerName `
+		        -Replace @{msExchHideFromAddressLists=$False}
 
-	    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated."
-	    $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated." >> $runningLog
-		#}
-	}
-	else 
-	{
-		#if ($isHidden -eq $False)
-		#{
-			$useraccount = Get-ADUser -Filter {sAMAccountName  -eq $name}
-			$useraccount2 = Get-ADUser -Filter {userPrincipalName  -eq $email}
+	        $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated."
+	        $count.ToString() + "`t" + $employeeId + "`t" + $sam + "`t" + $email + "`tADUser has been updated." >> $runningLog
+	    }
+        else
+        {
+		    $useraccount = Get-ADUser -Filter {sAMAccountName  -eq $name}
+		    $useraccount2 = Get-ADUser -Filter {userPrincipalName  -eq $email}
 
-			if($useraccount -ne $Null)
-			{
-				$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed."
-				$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed." >> $runningLog
-				$employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed." >> $syncBenqADLog
-			}
-			elseif($useraccount2 -ne $Null)
-			{
-				$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed."
-				$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed." >> $runningLog
-				$employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed." >> $syncBenqADLog
-			}
-			else
-			{
-				$ouPath = "OU="+ $ou +",OU=ADEN-Users,DC=CHOADEN,DC=COM"
-				if([adsi]::Exists("LDAP://$ouPath"))
-				{
-					New-ADUser $name `
-						-SamAccountName $name `
-						-userprincipalname $email `
-						-Surname $lastName `
-						-GivenName $firstName `
-						-DisplayName $displayName `
-						-Department $dept `
-						-Office $region `
-						-Title $jobTitle `
-						-OfficePhone $telephoneNumber `
-						-MobilePhone $mobilePhone `
-						-Initials $initials `
-						-EmailAddress $email `
-						-EmployeeID $employeeId `
-						-Manager $managerName `
-						-Path $ouPath   `
-						-AccountPassword (ConvertTo-SecureString "Aden@123" -AsPlainText -Force) `
-						-ChangePasswordAtLogon $false `
-						-enabled $true
-						$count.ToString() + $employeeId + "`t" + $name + "`t" + $email + "`thas been created."
-						$count.ToString() + $employeeId + "`t" + $name + "`t" + $email + "`thas been created." >> $runningLog
-						$employeeId + "`t" + $name + "`t" + $email + "`thas been created." >> $syncBenqADLog
-				}
-				else 
-				{
-					"OU path error. Script will terminate..." >> $syncBenqADLog
-					break
-				}
-			}
-		#}
-		<#else 
-		{
-			$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tADUser does not exist, but will be not created because user is set as hidden."
-			$count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tADUser does not exist, but will be not created because user is set as hidden." >> $runningLog
-		}#>
-	}
-	$count ++
+		    if($useraccount -ne $Null)
+		    {
+			    $count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed."
+			    $count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed." >> $runningLog
+			    $employeeId + "`t" + $name + "`t" + $email + "`tSAMACCOUNT already exists. Creation failed." >> $syncBenqADLog
+		    }
+		    elseif($useraccount2 -ne $Null)
+		    {
+			    $count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed."
+			    $count.ToString() + "`t" + $employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed." >> $runningLog
+			    $employeeId + "`t" + $name + "`t" + $email + "`tUPN already exists. Creation failed." >> $syncBenqADLog
+		    }
+		    else
+		    {
+			    $ouPath = "OU="+ $ou +",OU=ADEN-Users,DC=CHOADEN,DC=COM"
+			    if([adsi]::Exists("LDAP://$ouPath"))
+			    {
+				    New-ADUser $name `
+					    -SamAccountName $name `
+					    -userprincipalname $email `
+					    -Surname $lastName `
+					    -GivenName $firstName `
+					    -DisplayName $displayName `
+					    -Department $dept `
+					    -Office $region `
+					    -Title $jobTitle `
+					    -OfficePhone $telephoneNumber `
+					    -MobilePhone $mobilePhone `
+					    -Initials $initials `
+					    -EmailAddress $email `
+					    -EmployeeID $employeeId `
+					    -Manager $managerName `
+					    -Path $ouPath   `
+					    -AccountPassword (ConvertTo-SecureString "Aden@123" -AsPlainText -Force) `
+					    -ChangePasswordAtLogon $false `
+					    -enabled $true
+					    $count.ToString() + $employeeId + "`t" + $name + "`t" + $email + "`thas been created."
+					    $count.ToString() + $employeeId + "`t" + $name + "`t" + $email + "`thas been created." >> $runningLog
+					    $employeeId + "`t" + $name + "`t" + $email + "`thas been created." >> $syncBenqADLog
+			    }
+			    else 
+			    {
+				    "OU path error. Script will terminate..." >> $syncBenqADLog
+				    break
+			    }
+		    }
+        }
+    }
+    else
+    {
+        $count.ToString() + "`t" + $email + "`tis skipped for it's in the exclusion list."
+		$count.ToString() + "`t" + $email + "`tis skipped for it's in the exclusion list." >> $runningLog
+    }
+    $count ++
 }
 if (Test-Path $syncBenqADLog)
 {
